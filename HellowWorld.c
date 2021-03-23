1120bc8dc7755d0b51af5bbe8527f2303c723164
hjpark8907 <msyamun@gmail.com>
2021-03-23 17:01:02 +0900

    C ex1

---
+++ HellowWorld.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <float.h>
+int main(void)
+{
+	// ex 1
+//	printf("Hello, World!\n");
+//	return 0;
+
+	double dData = 123.456;
+
+	printf("%f\n", dData);
+	printf("%E - %E\n", DBL_MIN, DBL_MAX);
+	return 0;
+}
